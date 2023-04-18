ba8bace161406b6d9ec2eb7dc0a23cb67d9f357b
Taranis <solovyov.production@gmail.com>
2023-04-18 20:13:34 +0300

    Fixed SpellCaster class

--- Assets/Scripts/Spell/CastTypes/CastingProjectile.cs
+++ Assets/Scripts/Spell/CastTypes/CastingProjectile.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class CastingProjectile : MonoBehaviour
 {
-    public static void Cast(GameObject caster, Spell spell)
+    public static void Cast(Spell spell)
     {
         var projectile = Instantiate(spell.ProjectilePrefab);
         var spellProjectile = projectile.GetComponent<SpellProjectile>();
--- Assets/Scripts/Spell/SpellCaster.cs
+++ Assets/Scripts/Spell/SpellCaster.cs
@@ -1,5 +1,4 @@
 ﻿
-
 using UnityEngine;
 
 public static class SpellCaster
@@ -7,13 +6,13 @@ public static class SpellCaster
     public static void CastSpell(GameObject caster, Spell spell)
     {
         if (spell.Distance == 0) CastToSelf(caster, spell);
-        else if (spell.ProjectileSpeed > 0) CastProjectile(caster, spell);
+        else if (spell.ProjectileSpeed > 0) CastProjectile(spell);
         else CastRaycast(caster, spell);
     }
     
-    private static void CastProjectile(GameObject caster, Spell spell)
+    private static void CastProjectile(Spell spell)
     {
-        CastingProjectile.Cast(caster, spell);
+        CastingProjectile.Cast(spell);
     }
 
     private static void CastRaycast(GameObject caster, Spell spell)
