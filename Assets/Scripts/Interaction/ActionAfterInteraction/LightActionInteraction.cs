0c2d79d6be5329cf49c1939256d37f14785f5084
Taranis <solovyov.production@gmail.com>
2023-04-22 14:37:02 +0300

    Changed LightActionInteraction

--- Assets/Scripts/Interaction/ActionAfterInteraction/LightActionInteraction.cs
+++ Assets/Scripts/Interaction/ActionAfterInteraction/LightActionInteraction.cs
@@ -4,15 +4,26 @@ using UnityEngine.Rendering.Universal;
 
 public class LightActionInteraction : IActionAfterInterection
 {
+    [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Light2D light2D;
+    [SerializeField] private bool isLightningOnStart = true;
+    private bool isLightning = true;
 
     private void Start()
     {
         light2D = light2D == null?  gameObject.GetComponent<Light2D>() : light2D;
+        if (!isLightningOnStart) ChangeLightMode();
     }
 
     public override void Action(GameObject gameObject)
     {
+        ChangeLightMode();
+    }
+
+    private void ChangeLightMode()
+    {
         light2D.enabled = !light2D.enabled;
+        spriteRenderer.enabled = !isLightning;
+        isLightning = !isLightning;
     }
 }
